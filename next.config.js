31109ccc454daaeb5046326e62996f857d0bd979
hosan <fbghtks1000@gmail.com>
2025-04-02 14:58:46 +0900

    pwa 테스트

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // 기존 설정 유지
+};
+
+module.exports = withPWA(nextConfig); 
